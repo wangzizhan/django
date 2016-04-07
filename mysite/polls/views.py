af7847df7cfd5f2d781615209732c18b0abd9111
wangzizhan <geniuscat0524@gmail.com>
2016-04-07 16:27:57 +0800

    my first django app

---
+++ mysite/polls/views.py
@@ -0,0 +1,37 @@
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from .models import Question
+from django.template import loader
+from django.shortcuts import render, get_object_or_404
+from django.core.urlresolvers import reverse
+from django.views import generic
+# Create your views here.
+
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+	return Question.objects.order_by('-pub_date')[:5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+def vote(request, question_id):
+    #return HttpResponse("You're voting on question %s." % question_id)
+    p = get_object_or_404(Question, pk = question_id)
+    try:
+	selected_choice = p.choice_set.get(pk = request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+	return render(request, 'polls/detail.html', {
+	    'question': p,
+	    'error_message': "You didn't select a choice.",
+	})
+    else:
+	selected_choice.votes += 1
+	selected_choice.save()
+	return HttpResponseRedirect(reverse('polls:results', args = (p.id,)))
